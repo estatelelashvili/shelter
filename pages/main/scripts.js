5a8f311aa49ed4f937bef80016c69b46d71deb36
estatelelashvili <35972253+estatelelashvili@users.noreply.github.com>
2022-05-31 11:21:53 +0400

    feat: update main and pets pages

--- pages/main/scripts.js
+++ pages/main/scripts.js
@@ -11,3 +11,17 @@ document.getElementById("logo").onclick = function () {
 function chbg(target, color) {
   document.getElementById(target).style.backgroundColor = color;
 }
+
+function burgerFunction() {
+  let nav = document.getElementById("navy");
+
+  let ham = document.getElementById("hamburger");
+  if (window.innerWidth < 769) {
+    // alert(window.innerWidth);
+    nav.style.visibility = "hidden";
+    ham.style.visibility = "visible";
+  } else {
+    nav.style.visibility = "visible";
+    ham.style.visibility = "hidden";
+  }
+}
